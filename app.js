74f2957a9773aa2601774173f4c84bd6c84d947a
kbot-sa <keanenjacobs2@gmail.com>
2023-10-15 15:27:41 +0200

    final commit

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-let modal = document.querySelector('#modal-container');
+/*Variables*/
+
+let modalContainer = document.querySelector('#modal-container');
 let openButton = document.querySelector('#open-btn');
 let closeButton = document.querySelector('#close-btn');
+
+/*Event Listeners*/
+
+openButton.addEventListener('click', function() {
+        modalContainer.style.display = 'block';
+});
+
+closeButton.addEventListener('click', function() {
+    modalContainer.style.display = 'none'
+})
+
+window.addEventListener('click', function(e) { //This is used to click away the modalContainer if the user clicks anywhere besides the modal container
+    if (e.target === modalContainer) {
+        modalContainer.style.display = 'none';
+    }
+})
